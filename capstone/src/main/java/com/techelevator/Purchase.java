09c23c2b6b9b108c3118ef291bf98ddc9c61eb61
Dan Halsey <dvhalsey42@gmail.com>
2022-04-20 17:28:39 -0400

    Completed Capstone

--- capstone/src/main/java/com/techelevator/Purchase.java
+++ capstone/src/main/java/com/techelevator/Purchase.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Purchase extends Import{
+public class Purchase {
     private double totalMoney;
     NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
